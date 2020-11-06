6c69a2a20658328c0bccc2c0f27733b6b0ccd688
WenhaoCui <59945706+Wenhaocui@users.noreply.github.com>
2020-11-06 02:09:33 -0800

    Delete setup.sh

--- p2/setup.sh
+++
@@ -1,7 +0,0 @@
-sudo wget https://data.usc-ece.com/EE599/HW4/polyvore_outfits_hw.zip -P /mnt
-cd /mnt
-sudo unzip polyvore_outfits_hw.zip
-sudo chmod 777 polyvore_outfits
-
-source activate pytorch_latest_p36
-
